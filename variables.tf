998ea9b74a478811885d0ee7bfeb104a3c3a0eac
Magali <70401848+magrousseau@users.noreply.github.com>
2023-10-05 22:27:58 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
     type =  string 
-    description = "the name of your already existing resource group. It should be your github username"
+    description = "devops"
 }
 
 variable "location" {
