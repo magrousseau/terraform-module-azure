4563d5d67a3b4da4d54627329a37259872c67ebb
Magali <70401848+magrousseau@users.noreply.github.com>
2023-10-05 22:19:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.bastion")
+    public_key = file("~/.ssh/id_rsa.bastion.pub")
   }
 }
 
